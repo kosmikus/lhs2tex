932aa3d04cbd087a8fedf02c9f93fe4b9db9c182
andres <andres@d54a9f6e-e2dc-0310-8252-bfaaff2a01be>
2008-10-12 15:35:38 +0000

    * Forgot to change some version numbers.
    git-svn-id: https://subversion.cs.uu.nl/repos/staff.andres.lhs2tex/lhs2TeX/trunk@230 d54a9f6e-e2dc-0310-8252-bfaaff2a01be

--- Setup.hs
+++ Setup.hs
@@ -34,9 +34,9 @@ lhs2tex = "lhs2TeX"
 minPolytableVersion = [0,8,2]
 shortversion = show (numversion `div` 100) ++ "." ++ show (numversion `mod` 100)
 version = shortversion ++ if ispre then "pre" ++ show pre else ""
-numversion = 113
+numversion = 114
 ispre = False
-pre = 4
+pre = 1
 
 main = defaultMainWithHooks lhs2texHooks
 
