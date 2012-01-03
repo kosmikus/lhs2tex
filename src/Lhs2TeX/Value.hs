07563a99a74cdafb68e02419e58a13457df5e754
Andres Loeh <mail@andres-loeh.de>
2012-01-03 14:05:36 +0100

    Fixed a comment.

--- src/Lhs2TeX/Value.hs
+++ src/Lhs2TeX/Value.hs
@@ -4,7 +4,7 @@ import Lhs2TeX.Utils
 
 type Toggles = Trie Value
 
--- | Universal datatype for lhs2TeX run-time values
+-- | Universal datatype for lhs2TeX run-time values.
 data Value =
     Undef
   | Str   String
