18b62f3c0a429161cb150a58fd7884af774e2066
Andres Loeh <mail@andres-loeh.de>
2012-09-04 11:34:46 +0200

    Bump to 1.18.

--- Setup.hs
+++ Setup.hs
@@ -33,9 +33,9 @@ lhs2tex = "lhs2TeX"
 minPolytableVersion = [0,8,2]
 shortversion = show (numversion `div` 100) ++ "." ++ show (numversion `mod` 100)
 version = shortversion ++ if ispre then "pre" ++ show pre else ""
-numversion = 117
+numversion = 118
 ispre = False
-pre = 2
+pre = 1
 
 main = defaultMainWithHooks lhs2texHooks
 
