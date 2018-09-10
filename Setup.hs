bb03f2bb92ee004d194a64bf524e26d7d08c680c
Andres Loeh <andres@well-typed.com>
2018-09-10 13:51:59 +0200

    More version bumping.

--- Setup.hs
+++ Setup.hs
@@ -34,7 +34,7 @@ lhs2tex = "lhs2TeX"
 minPolytableVersion = [0,8,2]
 shortversion = show (numversion `div` 100) ++ "." ++ show (numversion `mod` 100)
 version = shortversion ++ if ispre then "pre" ++ show pre else ""
-numversion = 120
+numversion = 121
 ispre = False
 pre = 1
 
